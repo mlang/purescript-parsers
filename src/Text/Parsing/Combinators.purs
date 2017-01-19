baaea9da792e924d477d6ed31466dd55555e37b1
Mario Lang <mlang@delysid.org>
2017-01-19 21:29:19 +0100

    chainr, chainr1, oneOf

--- src/Text/Parsing/Combinators.purs
+++ src/Text/Parsing/Combinators.purs
@@ -1,12 +1,16 @@
 module Text.Parsing.Combinators (
-  between, either, option, optional, optionMaybe,
+  between, chainr, chainr1, either, oneOf, option, optional, optionMaybe,
   module Text.Parsing.Combinators.List
 ) where
 
 import Control.Alt (class Alt, (<|>))
 import Control.Applicative (class Applicative, pure)
-import Control.Apply (class Apply, (<*), (*>))
+import Control.Apply (class Apply, (<*), (<*>), (*>))
+import Control.Bind (class Bind, (>>=))
+import Control.Plus (class Plus)
 import Data.Either (Either(Left, Right))
+import Data.Foldable (class Foldable)
+import Data.Foldable (oneOf) as Foldable
 import Data.Function (($))
 import Data.Functor (class Functor, void, (<$>))
 import Data.Maybe (Maybe(Just, Nothing))
@@ -16,9 +20,23 @@ import Text.Parsing.Combinators.List (many, sepBy, sepBy1, some)
 between :: forall f open close a. Apply f => f open -> f close -> f a -> f a
 between open close p = open *> p <* close
 
+-- | Parse phrases delimited by a right-associative operator.
+chainr :: forall f a. (Alt f, Applicative f, Bind f)
+       => f a -> f (a -> a -> a) -> a -> f a
+chainr p f a = chainr1 p f <|> pure a
+
+-- | Parse phrases delimited by a right-associative operator,
+-- | requiring at least one match.
+chainr1 :: forall f a. (Alt f, Applicative f, Bind f)
+        => f a -> f (a -> a -> a) -> f a
+chainr1 p f = p >>= go where go a = ((_ $ a) <$> f <*> chainr1 p f) <|> pure a
+
 either :: forall f a b. (Alt f, Functor f)
        => f a -> f b -> f (Either a b)
 either a b = Left <$> a <|> Right <$> b
+
+oneOf :: forall f g a. (Foldable f, Plus g) => f (g a) -> g a
+oneOf = Foldable.oneOf
 
 option :: forall f a. (Alt f, Applicative f)
        => a -> f a -> f a
