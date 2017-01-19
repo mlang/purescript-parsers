4706f2d3d5d222ba2dec6a14ee6181c5bc1213f5
Mario Lang <mlang@delysid.org>
2017-01-19 14:39:17 +0100

    More combinators

--- src/Text/Parsing/Combinators.purs
+++ src/Text/Parsing/Combinators.purs
@@ -1,23 +1,42 @@
-module Text.Parsing.Combinators where
+module Text.Parsing.Combinators (
+  between, either, option, optional, optionMaybe, sepBy, sepBy1
+) where
 
 import Control.Alt (class Alt, (<|>))
+import Control.Alternative (class Alternative)
 import Control.Applicative (class Applicative, pure)
+import Control.Apply (class Apply, (<*), (<*>), (*>))
+import Control.Lazy (class Lazy)
 import Data.Either (Either(Left, Right))
 import Data.Function (($))
-import Data.Functor (void, (<$>))
+import Data.Functor (class Functor, void, (<$>))
+import Data.List (List(Nil), many, some, (:))
 import Data.Maybe (Maybe(Just, Nothing))
 import Data.Unit (Unit, unit)
 
-option :: forall f a. (Alt f, Applicative f) => a -> f a -> f a
+between :: forall f open close a. Apply f => f open -> f close -> f a -> f a
+between open close p = open *> p <* close
+
+either :: forall f a b. (Alt f, Functor f)
+       => f a -> f b -> f (Either a b)
+either a b = Left <$> a <|> Right <$> b
+
+option :: forall f a. (Alt f, Applicative f)
+       => a -> f a -> f a
 option a p = p <|> pure a
 
-optional :: forall f a. (Alt f, Applicative f) => f a -> f Unit
+optional :: forall f a. (Alt f, Applicative f)
+         => f a -> f Unit
 optional p = void p <|> pure unit
 
 optionMaybe :: forall f a. (Alt f, Applicative f)
             => f a -> f (Maybe a)
 optionMaybe p = option Nothing $ Just <$> p
 
-either :: forall f a b. (Alt f, Applicative f)
-       => f a -> f b -> f (Either a b)
-either a b = Left <$> a <|> Right <$> b
+sepBy :: forall f a sep. (Alternative f, Lazy (f (List a)))
+      => f a -> f sep -> f (List a)
+sepBy p sep = sepBy1 p sep <|> pure Nil
+
+sepBy1 :: forall f a sep. (Alternative f, Lazy (f (List a)))
+       => f a -> f sep -> f (List a)
+sepBy1 p sep = (:) <$> p <*> many (sep *> p)
