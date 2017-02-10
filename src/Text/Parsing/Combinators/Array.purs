25fad0c51b693f9582a377b318094ab2d7769ed9
Mario Lang <mlang@delysid.org>
2017-02-10 12:07:28 +0100

    Use option combinator.

--- src/Text/Parsing/Combinators/Array.purs
+++ src/Text/Parsing/Combinators/Array.purs
@@ -2,21 +2,21 @@ module Text.Parsing.Combinators.Array (
   many, sepBy, sepBy1, some
 ) where
 
-import Control.Alt ((<|>))
 import Control.Alternative (class Alternative)
-import Control.Applicative (pure)
 import Control.Apply ((<*>), (*>))
 import Control.Lazy (class Lazy)
 import Data.Functor ((<$>))
 import Data.Array ((:))
 import Data.Array (many, some) as Array
+import Data.Function (($))
+import Text.Parsing.Combinators (option)
 
 many :: forall f a. (Alternative f, Lazy (f (Array a))) => f a -> f (Array a)
 many = Array.many
 
 sepBy :: forall f a sep. (Alternative f, Lazy (f (Array a)))
       => f a -> f sep -> f (Array a)
-sepBy p sep = sepBy1 p sep <|> pure []
+sepBy p sep = option [] $ sepBy1 p sep
 
 sepBy1 :: forall f a sep. (Alternative f, Lazy (f (Array a)))
        => f a -> f sep -> f (Array a)
--- src/Text/Parsing/Combinators/List.purs
+++ src/Text/Parsing/Combinators/List.purs
@@ -2,21 +2,21 @@ module Text.Parsing.Combinators.List (
   many, sepBy, sepBy1, some
 ) where
 
-import Control.Alt ((<|>))
 import Control.Alternative (class Alternative)
-import Control.Applicative (pure)
 import Control.Apply ((<*>), (*>))
 import Control.Lazy (class Lazy)
+import Data.Function (($))
 import Data.Functor ((<$>))
 import Data.List (List(Cons, Nil))
 import Data.List (many, some) as List
+import Text.Parsing.Combinators (option)
 
 many :: forall f a. (Alternative f, Lazy (f (List a))) => f a -> f (List a)
 many = List.many
 
 sepBy :: forall f a sep. (Alternative f, Lazy (f (List a)))
       => f a -> f sep -> f (List a)
-sepBy p sep = sepBy1 p sep <|> pure Nil
+sepBy p sep = option Nil $ sepBy1 p sep
 
 sepBy1 :: forall f a sep. (Alternative f, Lazy (f (List a)))
        => f a -> f sep -> f (List a)
