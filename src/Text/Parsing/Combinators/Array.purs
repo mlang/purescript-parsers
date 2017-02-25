63b566ffc70e20e0865e77e8a8dcf6ae35f8d69d
Mario Lang <mlang@delysid.org>
2017-02-25 21:42:16 +0100

    sepEndBy, sepEndBy1

--- src/Text/Parsing/Combinators/Array.purs
+++ src/Text/Parsing/Combinators/Array.purs
@@ -1,14 +1,15 @@
 module Text.Parsing.Combinators.Array (
-  many, sepBy, sepBy1, some
+  many, sepBy, sepBy1, sepEndBy, sepEndBy1, some
 ) where
 
 import Control.Alternative (class Alternative)
+import Control.Applicative (pure)
 import Control.Apply ((<*>), (*>))
 import Control.Lazy (class Lazy)
-import Data.Functor ((<$>))
 import Data.Array ((:))
 import Data.Array (many, some) as Array
-import Data.Function (($))
+import Data.Function (flip, ($), (#))
+import Data.Functor ((<$>))
 import Text.Parsing.Combinators (option)
 
 many :: forall f a. (Alternative f, Lazy (f (Array a))) => f a -> f (Array a)
@@ -21,6 +22,14 @@ sepBy p sep = option [] $ sepBy1 p sep
 sepBy1 :: forall f a sep. (Alternative f, Lazy (f (Array a)))
        => f a -> f sep -> f (Array a)
 sepBy1 p sep = (:) <$> p <*> many (sep *> p)
+
+sepEndBy :: forall f a sep. (Alternative f, Lazy (f (Array a)))
+          => f a -> f sep -> f (Array a)
+sepEndBy p sep = option [] $ sepEndBy1 p sep
+
+sepEndBy1 :: forall f a sep. (Alternative f, Lazy (f (Array a)))
+          => f a -> f sep -> f (Array a)
+sepEndBy1 p sep = (#) <$> p <*> option pure (flip (:) <$> (sep *> sepEndBy p sep))
 
 some :: forall f a. (Alternative f, Lazy (f (Array a))) => f a -> f (Array a)
 some = Array.some
--- src/Text/Parsing/Combinators/List.purs
+++ src/Text/Parsing/Combinators/List.purs
@@ -1,11 +1,12 @@
 module Text.Parsing.Combinators.List (
-  many, sepBy, sepBy1, some
+  many, sepBy, sepBy1, sepEndBy, sepEndBy1, some
 ) where
 
 import Control.Alternative (class Alternative)
+import Control.Applicative (pure)
 import Control.Apply ((<*>), (*>))
 import Control.Lazy (class Lazy)
-import Data.Function (($))
+import Data.Function (flip, ($), (#))
 import Data.Functor ((<$>))
 import Data.List (List(Cons, Nil))
 import Data.List (many, some) as List
@@ -21,6 +22,14 @@ sepBy p sep = option Nil $ sepBy1 p sep
 sepBy1 :: forall f a sep. (Alternative f, Lazy (f (List a)))
        => f a -> f sep -> f (List a)
 sepBy1 p sep = Cons <$> p <*> many (sep *> p)
+
+sepEndBy :: forall f a sep. (Alternative f, Lazy (f (List a)))
+          => f a -> f sep -> f (List a)
+sepEndBy p sep = option Nil $ sepEndBy1 p sep
+
+sepEndBy1 :: forall f a sep. (Alternative f, Lazy (f (List a)))
+          => f a -> f sep -> f (List a)
+sepEndBy1 p sep = (#) <$> p <*> option pure (flip Cons <$> (sep *> sepEndBy p sep))
 
 some :: forall f a. (Alternative f, Lazy (f (List a))) => f a -> f (List a)
 some = List.some
